88675e4481992ee257e6f5fd535d6c43bc1fcf4a
Evgeny Tkachenko <evgeny.tk@mail.ru>
2019-03-24 00:08:08 +0300

    Added: callback url

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,14 +11,18 @@ class AuthController extends Controller
 {
     public function authenticate(Request $request)
     {
+        $this->validate($request, [
+            'callback_url' => 'required|url'
+        ]);
+
         $phone = PhoneNumber::where('number', $request->get('phone'))->first();
 
         if (!$phone) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Phone number not found', 
-                'data' => null
-            ], 401);
+            $data = http_build_query([
+                'success' => false
+            ]);
+
+            return redirect($request->get('callback_url').'?'.$data);
         }
 
         $user = $phone->user;
@@ -27,12 +31,12 @@ class AuthController extends Controller
             $user->generateToken();
         }
 
-        return response()->json([
+        $data = http_build_query([
             'success' => true,
-            'message' => "You're successfully identified",
-            'data' => [
-                'token' => $user->token
-            ]
-        ], 200);
+            'token' => $user->token,
+            'token_expired' => $user->token_expired
+        ]);
+
+        return redirect($request->get('callback_url').'?'.$data);
     }
 }
